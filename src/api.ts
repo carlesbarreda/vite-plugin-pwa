01351f53f846541e7d83ce89abebd3e7c43f40f0
Carles Barreda <cbm@carlesbarreda.cat>
2023-02-21 15:00:00 +0100

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -69,6 +69,9 @@ export function createAPI(ctx: PWAPluginContext): VitePluginPWAAPI {
       console.log(`options.mode: ${options.mode}`)
       console.log(`ctx.devEnvironment: ${ctx.devEnvironment}`)
       console.log(`ctx.options.devOptions.enabled: ${ctx.options.devOptions.enabled}`)
+      console.log('* import.meta.env')
+      console.log(import.meta.env)
+
       if (!options || options.disable || !options.manifest || (options.mode != 'production' && ctx.devEnvironment && !ctx.options.devOptions.enabled))
         return undefined
 
